08fdda4e3a8a607cff08ec8064ce00dc7a98b040
guoxiangke <xiangkeguo@gmail.com>
2020-02-10 19:35:44 +0800

    gts

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -35,16 +35,11 @@ window.Pusher = require('pusher-js');
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: process.env.MIX_PUSHER_APP_KEY,
+    key: 'anyKey',
+    // key: process.env.MIX_PUSHER_APP_KEY,
     // cluster: process.env.MIX_PUSHER_APP_CLUSTER,
     // encrypted: true
     wsHost: window.location.hostname,
     wsPort: 6001,
     disableStats: true,
 });
-
-
-window.Echo.channel('DemoChannelName')
-    .listen('WebsocketDemoEvent',(e)=>{
-        console.log(e);
-    }); 
