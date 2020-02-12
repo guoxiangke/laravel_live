601169c5a3b05a8f3eeb0366d5711c2dcb2b3c29
guoxiangke <xiangkeguo@gmail.com>
2020-02-12 17:14:02 +0800

    bug

--- app/Http/Controllers/Auth/LoginSocialController.php
+++ app/Http/Controllers/Auth/LoginSocialController.php
@@ -8,8 +8,10 @@ use App\Models\Social;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Hash;
 
 class LoginSocialController extends Controller
 {
@@ -29,12 +31,8 @@ class LoginSocialController extends Controller
     public function bindOrlogin($socialUser, $type)
     {
         $loginedId = Auth::id();
-        $social = Social::where('social_id', $socialUser->id)->first();
-        // \Log::error(__CLASS__, [__FUNCTION__, $social]);
         // \Log::error(__CLASS__, [__FUNCTION__, $type, $loginedId]);
         // \Log::error(__CLASS__, [__FUNCTION__, $socialUser]);
-
-
         if($loginedId){
             //用户已登录，执行绑定！
             Social::firstOrCreate([
@@ -45,25 +43,33 @@ class LoginSocialController extends Controller
                 'avatar'      => $socialUser->avatar,
             ]);
         }else{ //未登录，执行登录！
-            if($social){ //已绑定，并定期更新资料
+            $social = Social::where('social_id', $socialUser->id)->first();
+            if($social){ //已绑定，并定期更新头像
                 if($social->updated_at->diffInDays(now()) > 1) {
-                    $social->name = $socialUser->nickname ?: $socialUser->name;
+                    // $social->name = $name;
                     $social->avatar = $socialUser->avatar;
                     if($social->isDirty()){
                         $social->save();
                     }
                 }
             }else { //微信首次授权登录
+                $socialId = $socialUser->id;
+                $email = $socialId.'@wx.com';
+                $token = Str::random(10);
+                $password = Hash::make(Str::random(8));
                 $name = $socialUser->nickname ?: $socialUser->name;
-                $email = $name.'@wx.com';
                 $user = User::create([
                     'name' => $name,
                     'email' => $email,
+                    'email_verified_at' => now(),
+                    'password' => $password,
+                    'remember_token' => $token,
                 ]);
                 Social::create([
-                    'social_id' => $socialUser->id,
+                    'social_id' => $socialId,
                     'user_id'   => $user->id,
                     'type'      => $type,
+                    'sex'       => $socialUser->user->sex==1?1:0,
                     'name'      => $name,
                     'avatar'      => $socialUser->avatar,
                 ]);
