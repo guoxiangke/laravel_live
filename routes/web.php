2863f6bcb860c5d6da21ab0855b01a2278bfb731
guoxiangke <xiangkeguo@gmail.com>
2020-02-13 17:22:38 +0800

    test route

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,6 @@ Route::resources(['socials' => 'SocialController']);
 Route::get('login/wechat/callback', 'Auth\LoginSocialController@handleWechatProviderCallback')->name('login.weixin.callback');
 //login.weixin
 // 自动登录跳转 https://laracasts.com/discuss/channels/laravel/custom-login-page-redirection-from-middleware
-Route::get('login/wechat', 'Auth\LoginSocialController@redirectToWechatProvider')//->name('login.weixin');
-	->name('login');
-Route::get('login', 'Auth\LoginController@showLoginForm')
-	->name('login.default');
+Route::get('login/wechat', 'Auth\LoginSocialController@redirectToWechatProvider');//->name('login.weixin');
+	// ->name('login');
+// Route::get('login', 'Auth\LoginController@showLoginForm')->name('login.default');
